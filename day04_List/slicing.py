797465764d3625b0c22e17ab2487712a36d5be63
Kavitha G <144310452+kavitha1808@users.noreply.github.com>
2026-02-06 21:04:55 +0530

    Add slicing.py to demonstrate list slicing
    This script demonstrates various list slicing techniques in Python.

---
+++ day04_List/slicing.py
@@ -0,0 +1,12 @@
+# slicing.py
+# Demonstrates list slicing
+
+nums = [0, 1, 2, 3, 4, 5, 6, 7]
+
+print("Original list:", nums)
+
+print("nums[1:5]:", nums[1:5])
+print("nums[:4]:", nums[:4])
+print("nums[4:]:", nums[4:])
+print("nums[::2]:", nums[::2])
+print("nums[::-1] (reversed):", nums[::-1])
