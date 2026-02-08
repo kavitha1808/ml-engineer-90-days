9f96e3a337a4d1b9ee4905aa3ad20d4db2a25cfd
Kavitha G <144310452+kavitha1808@users.noreply.github.com>
2026-02-08 23:07:21 +0530

    Add phone book functionality in phone_book.py

---
+++ day06_dictionaries/phone_book.py
@@ -0,0 +1,14 @@
+# Mini Project 2: Phone Book
+
+phone_book = {
+    "Kavitha": "9876543210",
+    "Anu": "9123456780",
+    "Ravi": "9988776655"
+}
+
+name = "Kavitha"
+
+if name in phone_book:
+    print("Number:", phone_book[name])
+else:
+    print("Contact not found")
