45a107721a0d239bbfcfdf785f2762ee0d1ef6dc
Kavitha G <144310452+kavitha1808@users.noreply.github.com>
2026-02-08 23:08:03 +0530

    Add pass/fail analyzer for subjects based on scores

---
+++ day06_dictionaries/pass_fail_analyzer.py
@@ -0,0 +1,14 @@
+# Mini Project 6: Pass / Fail Analyzer
+
+marks = {
+    "Maths": 45,
+    "Science": 72,
+    "English": 30,
+    "Social": 66
+}
+
+for subject, score in marks.items():
+    if score >= 40:
+        print(subject, ": Pass")
+    else:
+        print(subject, ": Fail")
