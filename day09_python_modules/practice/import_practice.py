7b218fea52cf481b9b6a0136bbbfef3abd58a09c
Kavitha G <144310452+kavitha1808@users.noreply.github.com>
2026-02-11 22:18:18 +0530

    Create import_practice.py for import experiments
    Added a practice file to experiment with imports from math_utils and string_utils.

---
+++ day09_python_modules/practice/import_practice.py
@@ -0,0 +1,10 @@
+# practice/import_practice.py
+"""
+Practice file to experiment with imports
+"""
+
+from utils.math_utils import add
+from utils.string_utils import reverse_string
+
+print(add(3, 7))
+print(reverse_string("practice makes perfect"))
